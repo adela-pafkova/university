4dfe529d5afa4104f7b6eb778a4e72b0a4776d09
adela-pafkova <91271916+adela-pafkova@users.noreply.github.com>
2023-01-19 22:19:01 +0000

    Upload BEEC3 week 1 material
    R script + 4 datasets in csv format

---
+++ BEEC3 R basics.R
@@ -0,0 +1,6 @@
+# BEEC course stats tutorial 
+# Adéla Pafková, University of Edinburgh, s1978469@ed.ac.uk
+# Tutorial part 2 / loading data, plotting data, regression analysis
+
+rm(list=ls()) 
+library(ggplot2) 
